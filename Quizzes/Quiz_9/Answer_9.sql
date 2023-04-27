4e2dc88153cde5fa7a516827add5d761633019d0
Mehmet Emin Orhan <mehmeteminorhan99@gmail.com>
2023-04-28 00:11:08 +0300

    Quiz 9 solved

---
+++ Quizzes/Quiz_9/Answer_9.sql
@@ -0,0 +1,14 @@
+--Q1
+
+SELECT * FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+--Q2
+
+SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer 
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+--Q3
+
+SELECT customer.first_name, customer.last_name, rental.rental_id FROM customer 
+INNER JOIN rental ON rental.customer_id = customer.customer_id;
